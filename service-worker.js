29b2b4e06a8b7cf9c43d2a7771d8eb766c2d2243
Hilary <hilarylh.26@gmail.com>
2026-05-31 17:14:51 -0600

    eliminar dashboard

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'rsu-app-v32';
+const CACHE_NAME = 'rsu-app-v33';
 const APP_SHELL = [
     '/',
     '/index.html',
@@ -8,7 +8,6 @@ const APP_SHELL = [
     '/favoritos.html',
     '/ayuda.html',
     '/buzon.html',
-    '/dashboard.html',
     '/transacciones.html',
     '/chat.html',
     '/detalle_publicacion.html',
